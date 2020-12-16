5b7ab1862119a03256b543055f62c9a071308c47
raka3275 <59763304+raka3275@users.noreply.github.com>
2020-12-16 22:00:23 +0700

    Update function.php

--- function.php
+++ function.php
@@ -1,15 +1,17 @@
 <?php
+
 function request($url, $token = null, $data = null, $pin = null, $otpsetpin = null, $uuid = null){
+
 $header[] = "Host: api.gojekapi.com";
-$header[] = "User-Agent: okhttp/3.10.0";
+$header[] = "User-Agent: okhttp/3.12.1";
 $header[] = "Accept: application/json";
 $header[] = "Accept-Language: id-ID";
 $header[] = "Content-Type: application/json; charset=UTF-8";
-$header[] = "X-AppVersion: 3.40.2";
+$header[] = "X-AppVersion: 3.50.1";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -7.575488,110.824327";
+$header[] = "X-Location: -6.243684,106.978540";
 $header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
